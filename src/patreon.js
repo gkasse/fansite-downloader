329de40de56f31f03e1ea152d36c2db2bcefcebc
gkasse <kxb3317@gmail.com>
2025-04-20 23:33:22 +0900

    patreonの処理を修正

--- src/patreon.js
+++ src/patreon.js
@@ -6,62 +6,70 @@
  */
 const wait = time => new Promise(resolve => setTimeout(() => resolve(), time));
 
+const HEADER_CLASSNAME = 'byKBvM';
+const MAIN_CLASSNAME = 'biGFvI';
+
+/**
+ * 
+ * @param {HTMLImageElement[]} images 
+ * @returns {Promise<string[]>}
+ */
+async function parseLightboxImages(images) {
+  const result = [];
+  for (const image of images) {
+    image.click();
+    await wait(300);
+
+    const imageInBox = await new Promise(resolve => {
+      const id = setInterval(() => {
+        const tmp = document.querySelector('img[data-tag="lightboxImage"]');
+        if (!tmp) {
+          return;
+        }
+
+        clearInterval(id);
+        resolve(tmp);
+      }, 500);
+    });
+    result.push(imageInBox.src);
+
+    const close = await new Promise(resolve => {
+      const id = setInterval(() => {
+        const tmp = document.querySelector('*[data-tag="close"]');
+        if (!tmp) {
+          return;
+        }
+
+        clearInterval(id);
+        resolve(tmp);
+      }, 500);
+    });
+    close.click();
+    await wait(100);
+  }
+
+  return result;
+}
+
 browser.runtime.onMessage.addListener(async (msg) => {
   if (msg.text !== 'patreon') {
     return null;
   }
 
-  const pinnedImageUrls = [];
-  const chip = document.querySelector('*[data-tag="chip-container"]');
-  if (!!chip) {
-    const count = parseInt(chip.textContent, 10);
-    pinnedImageUrls.push(...await parseLightboxImages(count));
+  const images = [];
+  const pinedImages = document.querySelectorAll(`*[data-tag="post-card"] .${HEADER_CLASSNAME} img`);
+  if (pinedImages.length > 0) {
+    const pinedImageSources = await parseLightboxImages(pinedImages);
+    images.push(...pinedImageSources);
   }
 
-  const header = document.querySelector('.dVruMo');
-  if (!!header) {
-    header.querySelector('img').click();
-    await wait(500);
-    const img = document.querySelector('img[data-tag="lightboxImage"]');
-    pinnedImageUrls.push(img.src);
-
-    document.querySelector('button[data-tag="close"]').click();
-    await wait(500);
+  const mains = document.querySelectorAll(`.${MAIN_CLASSNAME} > div:nth-child(1) img`);
+  for (const body of mains) {
+    images.push(body.src);
   }
 
-  const nameHolder = document.querySelector('div[data-tag="metadata-wrapper"] > div:nth-child(1) > div');
-  const author = nameHolder?.textContent?.trim()?.replaceAll('"', '') ?? '';
-  const elements = document.querySelectorAll('.iATlLz img');
-  const images = Array.from(elements.values()).map(image => image.src);
+  const author = document.querySelector('div[data-tag="metadata-wrapper"] > div:nth-child(1) > div')?.textContent?.trim() ?? '';
+  const title = document.querySelector('span[data-tag="post-title"]')?.textContent?.trim() ?? '';
 
-  const title = document.querySelector('span[data-tag="post-title"]')
-    ?.textContent?.trim()?.replaceAll('"', '')?.replaceAll('?', '？') ?? '';
-  console.debug({title, author, images: [...pinnedImageUrls, ...images]});
-
-  return {title, author, images: [...pinnedImageUrls, ...images]};
+  return { title, author, images };
 });
-
-/**
- * @param {number} count
- * @return string[]
- */
-async function parseLightboxImages(count) {
-  const result = [];
-  document.querySelector('.dBNGew img').click();
-  await wait(500);
-
-  let img = document.querySelector('img[data-tag="lightboxImage"]');
-  result.push(img.src);
-
-  for (let i = 1; i < count; i++) {
-    document.querySelector('button[data-tag="nextImage"]').click();
-    await wait(500);
-    img = document.querySelector('img[data-tag="lightboxImage"]');
-    result.push(img.src);
-  }
-
-  document.querySelector('button[data-tag="close"]').click();
-  await wait(500);
-
-  return result;
-}
